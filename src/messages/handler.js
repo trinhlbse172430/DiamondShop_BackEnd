d24995e779dc974d10c55c28bdcaa1383023bc61
LeBaLu <lelu532003@gmail.com>
2024-06-25 08:08:50 +0700

    Bonus_point_CRUD

---
+++ src/messages/handler.js
@@ -0,0 +1,11 @@
+const CODE_401 = "Không được phép truy cập";
+const CODE_403 = "Không có quyền truy cập";
+const CODE_404 = "Không tồn tại tài nguyên";
+const CODE_500 = "Xảy ra lỗi trên hệ thống";
+
+module.exports = {
+    CODE_401,
+    CODE_403,
+    CODE_404,
+    CODE_500,
+};
