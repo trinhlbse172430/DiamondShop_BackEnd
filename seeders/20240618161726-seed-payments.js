32848b1cba7d90aa9f741e296c446d60dacd80f5
lebuutrinh <trinhlbse172430@fpt.edu.vn>
2024-06-28 00:40:59 +0700

    seeder_Ver4.2

--- seeders/20240618161726-seed-payments.js
+++ seeders/20240618161726-seed-payments.js
@@ -8,10 +8,10 @@ module.exports = {
 				{
 					PaymentID: 1,
 					InputDate: "2024-06-18",
-					InputEmployeeID: "EMP001",
+					InputEmployeeID: "EMP003",
 					OrderID: "ORD001",
 					PayType: "Chuyển khoản",
-					Pay: 19000000.0,
+					Pay: 17550000.0,
 					Currency: "VND",
 				},
 			],
